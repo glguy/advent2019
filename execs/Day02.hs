5297eb018f82f38e13b6118a90a68359e5695d65
Eric Mertens <emertens@galois.com>
2019-12-02 12:57:54 -0800

    Update Day02.hs

--- execs/Day02.hs
+++ execs/Day02.hs
@@ -1,4 +1,3 @@
-{-# Options_GHC -w #-}
 {-# Language OverloadedStrings #-}
 {-|
 Module      : Main
@@ -12,22 +11,24 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import           Advent (getParsedLines, number, sepBy)
+import           Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 
-type Program = IntMap Int
+type Memory = Seq Int
 
-(!) :: Program -> Int -> Int
-(!) = (IntMap.!)
+(!) :: Memory -> Int -> Int
+(!) = Seq.index
 
-newProgram :: [Int] -> Program
-newProgram = IntMap.fromList . zip [0..]
+new :: [Int] -> Memory
+new = Seq.fromList
+
+set :: Int -> Int -> Memory -> Memory
+set i v m = v `seq` Seq.update i v m
 
 main :: IO ()
 main =
-  do inp <- getParsedInput 2 (number `sepBy` ",")
-     let pgm = newProgram inp
+  do [pgm] <- getParsedLines 2 (new <$> number `sepBy` ",")
      print (startup 12 2 pgm)
      print (head [ 100 * noun + verb
                  | noun <- [0..99]
@@ -35,21 +36,51 @@ main =
                  , startup noun verb pgm == 19690720 ])
 
 -- | Run the given program after assigning the given noun and verb.
-startup :: Int {- ^ noun -} -> Int {- ^ verb -} -> Program -> Int
+startup :: Int {- ^ noun -} -> Int {- ^ verb -} -> Memory -> Int
 startup noun verb
-  = run 0
-  . IntMap.insert 1 noun
-  . IntMap.insert 2 verb
+  = (! 0)
+  . run 0
+  . Seq.update 1 noun
+  . Seq.update 2 verb
 
 -- | Run the given program starting at the given program counter
 -- returning the initial memory value once the program halts.
-run :: Int {- ^ program counter -} -> Program -> Int
-run i pgm =
-  case arg 0 of
-    1  -> math (+)
-    2  -> math (*)
-    99 -> pgm ! 0
+--
+-- >>> let check = Data.Foldable.toList . run 0 . new
+-- >>> check [1,0,0,0,99]
+-- [2,0,0,0,99]
+-- >>> check [2,3,0,3,99]
+-- [2,3,0,6,99]
+-- >>> check [2,4,4,5,99,0]
+-- [2,4,4,5,99,9801]
+-- >>> check [1,1,1,4,99,5,6,0,99]
+-- [30,1,1,4,2,5,6,0,99]
+-- >>> check [1,9,10,3,2,3,11,0,99,30,40,50]
+-- [3500,9,10,70,2,3,11,0,99,30,40,50]
+run :: Int -> Memory -> Memory
+run i pgm = last (programTrace i pgm)
+
+-- | Run a program providing a list of intermediate states of the program.
+--
+-- >>> let pgm = [1,9,10,3,2,3,11,0,99,30,40,50]
+-- >>> mapM_ (print . Data.Foldable.toList) (programTrace 0 (new pgm))
+-- [1,9,10,3,2,3,11,0,99,30,40,50]
+-- [1,9,10,70,2,3,11,0,99,30,40,50]
+-- [3500,9,10,70,2,3,11,0,99,30,40,50]
+programTrace :: Int {- ^ program counter -} -> Memory -> [Memory]
+programTrace pc pgm = pgm : rest
   where
-    math f = run (i+4) (IntMap.insert (arg 3) (val 1 `f` val 2) pgm)
-    val j = pgm ! arg j
-    arg j = pgm ! (i+j)
+    op2 (?) = programTrace (pc + 4) (set (arg 3) (val 1 ? val 2) pgm)
+
+    -- Opcode argument
+    arg i = pgm ! (pc + i)
+
+    -- Dereferenced opcode argument
+    val i = pgm ! arg i
+
+    rest =
+      case arg 0 of
+        1  -> op2 (+)
+        2  -> op2 (*)
+        99 -> []
+        o  -> error ("Bad opcode " ++ show o ++ " at " ++ show pc)
