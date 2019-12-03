8df2c4f73ddb9407bac63beff8863c550f7d714f
Eric Mertens <emertens@galois.com>
2019-12-03 13:28:11 -0800

    Update Day03.hs

--- execs/Day03.hs
+++ execs/Day03.hs
@@ -12,8 +12,6 @@ Maintainer  : emertens@gmail.com
 module Main (main) where
 
 import           Advent (Parser, sepBy, getParsedLines, anySingle, number)
-import           Advent.Coord (Coord)
-import qualified Advent.Coord as Coord
 import           Control.Applicative (liftA2)
 import           Data.Foldable (asum)
 import           Data.List (scanl', foldl1')
@@ -29,6 +27,8 @@ data Motion = Motion Direction Int
 data Direction = U | L | D | R
   deriving Show
 
+type Coord = (Int,Int)
+
 parseDirection :: Parser Direction
 parseDirection = asum [U <$ "U", D <$ "D", R <$ "R", L <$ "L"]
 
@@ -40,6 +40,22 @@ main =
   do (p1,p2) <- answers <$> getParsedLines 3 parseSteps
      print p1
      print p2
+
+-- | Pair-wise addition of coordinates.
+--
+-- >>> addCoord (1,2) (3,4)
+-- (4,6)
+addCoord :: Coord -> Coord -> Coord
+addCoord (x,y) (dx,dy) = (x+dx, y+dy)
+
+-- | Manhattan distance from origin for a given coordinate
+--
+-- >>> manhattan (3,4)
+-- 7
+-- >>> manhattan (-3,-5)
+-- 8
+manhattan :: Coord -> Int
+manhattan (x,y) = abs x + abs y
 
 -- | Given the input file parsed as lists of lists of motions, compute the
 -- nearest distance to origin and minimum sum steps to intersection.
@@ -59,7 +75,7 @@ answers xs = (nearestDistanceToOrigin intersections, minimum intersections)
 -- | Computes the map of path intersections, compute the shortest
 -- distance of an intersection to the origin.
 nearestDistanceToOrigin :: Map Coord a -> Int
-nearestDistanceToOrigin = minimum . map (Coord.manhattan Coord.origin) . Map.keys
+nearestDistanceToOrigin = minimum . map manhattan . Map.keys
 
 -- | Given a list of paths compute a map of locations that have intersections
 -- among all of the paths. The value at each location is the sum of the
@@ -67,36 +83,31 @@ nearestDistanceToOrigin = minimum . map (Coord.manhattan Coord.origin) . Map.key
 --
 -- >>> let check = pathIntersections . parse
 -- >>> check ["R8,U5,L5,D3","U7,R6,D4,L4"]
--- fromList [(C (-5) 6,30),(C (-3) 3,40)]
+-- fromList [((3,-3),40),((6,-5),30)]
 pathIntersections :: [[Motion]] -> Map Coord Int
 pathIntersections = foldl1' (Map.intersectionWith (+)) . map locations
 
 -- | Generate a map of the coordinates a path visits. Each coordinate is
 -- indexed by the number of steps it took to get to that location.
 --
--- >>> locations [Motion 'D' 2, Motion 'R' 1]
--- fromList [(C 1 0,1),(C 2 0,2),(C 2 1,3)]
+-- >>> locations [Motion D 2, Motion R 1]
+-- fromList [((0,1),1),((0,2),2),((1,2),3)]
 locations :: [Motion] -> Map Coord Int
-locations steps = Map.fromListWith min (zip (generatePath Coord.origin steps) [1..])
+locations steps = Map.fromListWith min (zip (generatePath steps) [1..])
 
 -- | Generate the list of coordinates visited by a list of steps.
 --
--- >>> generatePath Coord.origin [Motion 'D' 2, Motion 'R' 1]
--- [C 1 0,C 2 0,C 2 1]
-generatePath :: Coord -> [Motion] -> [Coord]
-generatePath c xs
+-- >>> generatePath [Motion D 2, Motion R 1]
+-- [(0,1),(0,2),(1,2)]
+generatePath :: [Motion] -> [Coord]
+generatePath
   = tail -- drop starting point
-  $ scanl' (\x f -> f x) c
-  $ concatMap (\(Motion d n) -> replicate n (todir d)) xs
+  . scanl' addCoord (0,0)
+  . concatMap (\(Motion d n) -> replicate n (toUnitVector d))
 
--- | Convert a direction letter to a coordinate update function.
---
--- >>> todir R Coord.origin
--- C 0 1
--- >>> todir D Coord.origin
--- C 1 0
-todir :: Direction -> Coord -> Coord
-todir L = Coord.left
-todir R = Coord.right
-todir U = Coord.above
-todir D = Coord.below
+-- | Convert a direction letter unit vector in the given direction.
+toUnitVector :: Direction -> Coord
+toUnitVector U = ( 0, -1)
+toUnitVector D = ( 0,  1)
+toUnitVector L = (-1,  0)
+toUnitVector R = ( 1,  0)
