cb922a72a70799c5990b5123673948c67d8e21fc
Eric Mertens <emertens@gmail.com>
2019-12-08 12:48:19 -0800

    Update Day08.hs

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,3 +1,4 @@
+{-# Language OverloadedStrings #-}
 {-|
 Module      : Main
 Description : Day 8 solution
@@ -10,33 +11,42 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent              (getParsedLines, anySingle, count)
-import Control.Applicative (many)
+import Advent              (Parser, getParsedLines, count)
+import Control.Applicative ((<|>),many)
 import Data.List           (minimumBy)
 import Data.Ord            (comparing)
 
+type Layer = [[Pixel]]
+
+data Pixel = Black | White | Transparent
+  deriving (Eq, Ord, Show)
+
+parsePixel :: Parser Pixel
+parsePixel = Black <$ "0" <|> White <$ "1" <|> Transparent <$ "2"
+
 main :: IO ()
 main =
-  do [inp] <- getParsedLines 8 (many anySingle)
+  do [inp] <- getParsedLines 8 (many parsePixel)
      let layers = chunks 6 (chunks 25 inp)
      print (part1 layers)
      mapM_ (putStrLn . map render) (overlayLayers layers)
 
-render :: Char -> Char
-render '1' = 'X'
-render _   = ' '
+render :: Pixel -> Char
+render Black       = '\x2591'
+render Transparent = '\x2592'
+render White       = '\x2593'
 
-overlayLayers :: [[[Char]]] -> [[Char]]
+overlayLayers :: [Layer] -> Layer
 overlayLayers = foldr1 (zipWith (zipWith overlay))
 
-overlay :: Char -> Char -> Char
-overlay '2' y = y
-overlay x   _ = x
+overlay :: Pixel -> Pixel -> Pixel
+overlay Transparent x = x
+overlay x           _ = x
 
-part1 :: [[[Char]]] -> Int
-part1 layers = count ('1'==) layer * count ('2'==) layer
+part1 :: [Layer] -> Int
+part1 layers = count (White==) layer * count (Transparent==) layer
   where
-    layer = minimumBy (comparing (count ('0'==)))
+    layer = minimumBy (comparing (count (Black==)))
           $ map concat layers
 
 chunks :: Int -> [a] -> [[a]]
