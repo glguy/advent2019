ea02e21804a50b1b172c3c1b27f77b2f34e18ab0
Eric Mertens <emertens@gmail.com>
2019-12-10 07:52:46 -0800

    Update Day10.hs

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -17,7 +17,7 @@ import           Data.List
 import           Data.Foldable
 import           Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Ord
+import           Data.Ord (comparing)
 
 main :: IO ()
 main =
@@ -25,48 +25,42 @@ main =
 
      let m :: Set (Int,Int)
          m = Set.fromList
-                [ ((c,r)) | (r,row) <- zip [0..] inp, (c,True) <- zip [0..] row ]
+                [ ((x,y)) | (y,row) <- zip [0..] inp, (x,True) <- zip [0..] row ]
 
-     let (base, vis) = maximumBy (comparing snd) $ [ (i, count (visible m i) m) | i <-  toList m ]
+     let (base, vis) = maximumBy (comparing snd) [ (i, count (visible m i) m) | i <-  toList m ]
      print vis
      let (x,y) = part2 base (Set.delete base m) !! 199
      print (x * 100 + y)
-
 
 part2 :: (Int,Int) -> Set (Int,Int) -> [(Int,Int)]
 part2 base m
   | Set.null m = []
   | otherwise  = these ++ part2 base (Set.difference m (Set.fromList these))
   where
-    these = filter (visible m base) (sortOn (angle . sub base) ((toList m)))
+    these = filter (visible m base) (sortOn (angle . sub base) (toList m))
 
 sub :: (Int,Int) -> (Int,Int) -> (Int,Int)
 sub (x,y) (u,v) = (u-x, v-y)
 
+-- |
+--
+-- >>> angle (0,-1) < angle (1,0)
+-- True
+-- >>> angle (1,0) < angle (0,1)
+-- True
+-- >>> angle (0,1) < angle (-1,0)
+-- True
 angle :: (Int,Int) -> Double
-angle (x,y) = angle' (x,-y)
-
-angle' :: (Int,Int) -> Double
-angle' (x,y)
-  | x == 0, y >= 0 = 0
-  | x >  0, y >= 0 = pi/2 - atan (fromIntegral y / fromIntegral x)
-  | x >  0, y <  0 = pi/2 - atan (fromIntegral y / fromIntegral x)
-  | x == 0, y <  0 = pi
-  | x <  0, y <  0 = 3*pi/2 - atan (fromIntegral y / fromIntegral x)
-  | otherwise      = 3*pi/2 - atan (fromIntegral y / fromIntegral x)
+angle (x,y) = atan2 (fromIntegral y) (fromIntegral x)
 
 visible :: Set (Int, Int) -> (Int, Int) -> (Int, Int) -> Bool
 visible _ x y | x == y = False
 visible ast (x,y) (u,v) =
-  not $
-  any (`Set.member` ast)
-      [ (u + stepx * i, v + stepy * i)   | i <- [1 .. n-1] ]
+  and [ Set.notMember (u + stepx * i, v + stepy * i) ast | i <- [1 .. steps-1] ]
   where
     dx = x - u
     dy = y - v
-    step = gcd dx dy
-    n | dx /= 0   = dx `div` stepx
-      | otherwise = dy `div` stepy
+    steps = gcd dx dy
 
-    stepx = dx `div` step
-    stepy = dy `div` step
+    stepx = dx `div` steps
+    stepy = dy `div` steps
