564b240923d64298aeded557937358358f7bc8c4
Eric Mertens <emertens@gmail.com>
2019-12-15 12:57:01 -0800

    Update Search.hs

--- common/Advent/Search.hs
+++ common/Advent/Search.hs
@@ -16,11 +16,18 @@ import qualified Data.IntSet as IntSet
 
 {-# INLINE dfs #-}
 dfs :: Ord a => (a -> [a]) -> a -> [a]
-dfs next start = aux start (const []) Set.empty
+dfs = dfsOn id
+
+dfsOn :: Ord r => (a -> r) -> (a -> [a]) -> a -> [a]
+dfsOn rep next start = loop Set.empty [start]
   where
-    aux x rest seen
-      | Set.member x seen = rest seen
-      | otherwise = x : foldr aux rest (next x) (Set.insert x seen)
+    loop _ [] = []
+    loop seen (x:xs)
+      | Set.member r seen =     loop seen xs
+      | otherwise         = x : loop seen1 (next x ++ xs)
+      where
+        r     = rep x
+        seen1 = Set.insert r seen
 
 {-# INLINE bfs #-}
 bfs :: Ord a => (a -> [a]) -> a -> [a]
@@ -41,16 +48,14 @@ bfsOn ::
   [a]        {- ^ reachable states          -}
 bfsOn rep next start = loop Set.empty (Queue.singleton start)
   where
-    loop seen q =
-      case q of
-        Queue.Empty -> []
-        x Queue.:<| q1
-          | Set.member r seen ->     loop seen  q1
-          | otherwise         -> x : loop seen1 q2
-          where
-            r     = rep x
-            seen1 = Set.insert r seen
-            q2    = Queue.appendList (next x) q1
+    loop _ Queue.Empty = []
+    loop seen (x Queue.:<| q1)
+      | Set.member r seen =     loop seen  q1
+      | otherwise         = x : loop seen1 q2
+      where
+        r     = rep x
+        seen1 = Set.insert r seen
+        q2    = Queue.appendList (next x) q1
 
 {-# RULES "bfsOn/Int" bfsOn = bfsOnInt #-}
 {-# INLINE bfsOnInt #-}
