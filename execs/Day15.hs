1a46b5d82eafb12ecd97539ada072c5e29d4aeb0
Eric Mertens <emertens@gmail.com>
2019-12-15 10:40:00 -0800

    Update Day15.hs

--- execs/Day15.hs
+++ execs/Day15.hs
@@ -16,16 +16,6 @@ import Advent.Coord   (Coord, above, below, left, right, origin)
 import Advent.Intcode (Effect(..), run, new, memoryParser)
 import Advent.Search  (bfsOn)
 
-data SearchState = SearchState
-  { onOxygen :: !Bool  -- ^ Is the robot currently on the oxygen
-  , distance :: !Int   -- ^ Commands issued so far
-  , location :: !Coord -- ^ robot's current location
-  , effect   :: Effect -- ^ robot control program state
-  }
-
-newSearchState :: [Integer] {- ^ intcode -} -> SearchState
-newSearchState = SearchState False 0 origin . run . new
-
 main :: IO ()
 main =
   do [intcode] <- getParsedLines 15 memoryParser
@@ -33,16 +23,27 @@ main =
      print (distance part1)
      print (distance (last (explore part1{distance = 0})))
 
+data SearchState = SearchState
+  { onOxygen :: !Bool  -- ^ Is the robot currently on the oxygen
+  , distance :: !Int   -- ^ Commands issued so far
+  , location :: !Coord -- ^ robot's current location
+  , effect   :: Effect -- ^ robot control program state
+  }
+
+-- | Initial search state starting from assumed non-oxygen at the origin.
+newSearchState :: [Integer] {- ^ intcode -} -> SearchState
+newSearchState = SearchState False 0 origin . run . new
+
 -- | Breadth-first exploration of the maze
 explore :: SearchState -> [SearchState]
-explore = bfsOn location step1
+explore = bfsOn location singleStep
 
--- | Advance a robot one step, update its location
-step1 :: SearchState -> [SearchState]
-step1 SearchState{..} =
+-- | Generate the list of single steps that can be taken from a particular position.
+singleStep :: SearchState -> [SearchState]
+singleStep SearchState{..} =
   [ SearchState (o == 2) (distance + 1) (move location) e
     | Input f     <- [effect]
     , (i,move)    <- [(1,above),(2,below),(3,left),(4,right)]
     , Output o e  <- [f i]
-    , o == 1 || o == 2
+    , o > 0
     ]
