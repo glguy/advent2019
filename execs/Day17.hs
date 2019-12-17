51b773cd30202025dcce41d6be454791e46277f2
Eric Mertens <emertens@galois.com>
2019-12-17 09:39:57 -0800

    Update Day17.hs

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -17,10 +17,12 @@ import           Data.Char
 import           Data.Map (Map)
 import qualified Data.Map as Map
 
+import Data.List
+import Data.Maybe
+
 main :: IO ()
 main =
   do [inp] <- getParsedLines 17 memoryParser
-
      let ascii = map (chr . fromIntegral) (intCodeToList inp [])
          world = Map.fromList [ (C y x, col)
                                 | (y,row) <- zip [0..] (lines ascii)
@@ -30,43 +32,69 @@ main =
                  | k <- Map.keys world
                  , all (\x -> '#' == at x world) (k : cardinal k) ]
 
-     print $ last $ effectList (run (set 0 2 (new inp)))
-                  $ map (fromIntegral . ord) script
-
-script :: String
-script = unlines [
-  "A,B,A,C,B,C,A,B,A,C",
-  "R,10,L,8,R,10,R,4",
-  "L,6,L,6,R,10",
-  "L,6,R,12,R,12,R,10",
-  "n"]
+     print (part2 inp world)
 
 at :: Coord -> Map Coord Char -> Char
 at = Map.findWithDefault '.'
 
-
-{-
-let start = head [ k | (k,'^') <- Map.toList world ]
-print $ intercalate "," $ "R" : path world start (C 0 1)
-
-path world here dir = next
+part2 :: [Integer] -> Map Coord Char -> Integer
+part2 inp world =
+     last $ effectList (run (set 0 2 (new inp)))
+          $ map (fromIntegral . ord) input
   where
-    endPoint = last steps
-    n = length steps - 1
-    steps = takeWhile (\x -> at x world /= '.')
-                    $ iterate (addCoord dir) here
+    start   = head [ k | (k,'^') <- Map.toList world ]
+    p       = path world start (C (-1) 0)
+    input:_ = compress [] Nothing Nothing Nothing p
 
-    next
-      | '#' == at (addCoord (turnLeft dir) endPoint) world =
-           show n : "L" : path world endPoint (turnLeft dir)
-      | '#' == at (addCoord (turnRight dir) endPoint) world =
-           show n : "R" : path world endPoint (turnRight dir)
-      | otherwise = show n : []
+data Dir = U | D | L | R
+  deriving (Eq, Ord, Show)
+
+path :: Map Coord Char -> Coord -> Coord -> [(Dir,Int)]
+path world here dir
+  | '#' == at (addCoord (turnLeft  dir) here) world = walk L turnLeft
+  | '#' == at (addCoord (turnRight dir) here) world = walk R turnRight
+  | otherwise                                           = []
+  where
+    walk cmd f = (cmd, n) : path world endPoint dir'
+      where
+        dir'     = f dir
+        steps    = takeWhile (\x -> at x world /= '.')
+                 $ iterate (addCoord dir') here
+        n        = length steps - 1
+        endPoint = last steps
+
+compress ::
+  [String] ->
+  Maybe [(Dir,Int)] ->
+  Maybe [(Dir,Int)] ->
+  Maybe [(Dir,Int)] ->
+  [(Dir,Int)] ->
+  [String]
+compress acc a b c [] = [unlines [ intercalate "," (reverse acc)
+                                 , maybe "L" instructions a
+                                 , maybe "L" instructions b
+                                 , maybe "L" instructions c, "n"] ]
+compress acc a b c xs =
+  do (ys,zs) <- splits xs
+     [ () | short ys ]
+     id [ xs | Just ys == a, xs <- compress ("A":acc) a b c zs ] ++
+        [ xs | Just ys == b, xs <- compress ("B":acc) a b c zs ] ++
+        [ xs | Just ys == c, xs <- compress ("C":acc) a b c zs ] ++
+        [ xs | isNothing a,                           xs <- compress ("A":acc) (Just ys) b c zs ] ++
+        [ xs | isJust    a, isNothing b,              xs <- compress ("B":acc) a (Just ys) c zs ] ++
+        [ xs | isJust    a, isJust    b, isNothing c, xs <- compress ("C":acc) a b (Just ys) zs ]
+
+short :: [(Dir,Int)] -> Bool
+short xs = length (instructions xs) <= 20
+
+instructions :: [(Dir,Int)] -> String
+instructions xs = intercalate "," [ show d ++ "," ++ show n | (d,n) <- xs ]
+
+splits :: [a] -> [([a],[a])]
+splits xs = tail $ zip (inits xs) (tails xs)
 
 play :: Effect -> IO ()
 play (Output i Halt) = putStrLn ("<<" ++ show i ++ ">>")
 play (Output o e) = putChar (chr (fromIntegral o)) >> play e
 play (Input f) = getChar >>= play . f . fromIntegral . ord
 play Halt = return ()
-
--}
