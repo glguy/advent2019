2bf9775a9304430b70728251231021a750102ccf
Eric Mertens <emertens@galois.com>
2019-12-18 09:08:23 -0800

    Improve 18

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -1,4 +1,4 @@
-{-# Language RecordWildCards, OverloadedStrings #-}
+{-# Language RecordWildCards #-}
 {-|
 Module      : Main
 Description : Day 18 solution
@@ -8,9 +8,8 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2019/day/18>
 
-This solution used roughly 1GB of RAM and took
-156 seconds to run both parts but was good enough
-to finish the problem!
+This solution used roughly 40MB of RAM and runs
+in 40 seconds on my laptop.
 
 -}
 module Main (main) where
@@ -21,55 +20,72 @@ import           Advent.Search
 import           Control.Applicative
 import           Data.List
 import           Data.Char
-import qualified Data.Map as Map
 import           Data.Set (Set)
 import qualified Data.Set as Set
+import           Control.Monad
+import           Data.Array.Unboxed
 
 main :: IO ()
 main =
   do inp <- getParsedLines 18 (many anySingle)
-     let world = Map.fromList [ (C y x, col)
-                                | (y,row) <- zip [0..] inp
-                                , (x,col) <- zip [0..] row ]
-     let start = head [ k | (k,'@') <- Map.toList world ]
+     let inp1 = [ (C y x, col)
+                 | (y,row) <- zip [0..] inp
+                 , (x,col) <- zip [0..] row ]
+         Just b = boundingBox (map fst inp1)
+         world = listArray b (map snd inp1)
+               :: UArray Coord Char
 
-     let keyN = Map.size (Map.filter isLower world)
+     let start = head [ k | (k,'@') <- inp1 ]
+     let keyN = length (filter isLower (concat inp))
 
-     print $ fmap steps
-           $ find (done keyN)
-           $ bfsOn (\ss -> (keys ss, location ss)) (step1 world) (ss0 start [])
+     print $ find (\s -> Set.size (akKeys (fst s)) == keyN) $ allKeys world (Set.singleton start)
 
-     let world2 = foldl' (\m k -> Map.insert k '#' m) world (start : cardinal start)
-         starts = [ left (above start), right (above start),
+     let world2 = world // [ (c,'#')  | c <- start : cardinal start ]
+         start2 = Set.fromList
+                  [ left (above start), right (above start),
                     left (below start), right (below start) ]
 
-     print $ fmap steps
-           $ find (done keyN)
-           $ bfsOnN (\ss -> (keys ss, location ss)) (step1 world2)
-                [ ss0 x (delete x starts) | x <- starts]
+     print $ find (\s -> Set.size (akKeys (fst s)) == keyN) $ allKeys world2 start2
 
-data SearchState = SS
-  { steps :: !Int
-  , keys  :: !(Set Char)
-  , location :: !Coord
-  , others   :: [Coord]
+data AllKeys = AllKeys
+  { akLocation :: !(Set Coord)
+  , akKeys     :: !(Set Char)
+  }
+  deriving (Ord, Eq, Show)
+
+allKeys :: UArray Coord Char -> Set Coord -> [(AllKeys, Int)]
+allKeys world start =
+  astar stepAK (AllKeys start Set.empty)
+  where
+    stepAK AllKeys{..} =
+      do who   <- Set.toList akLocation
+         (s,k) <- nextKeys world akKeys who
+         [ (AllKeys
+             (Set.insert (fkLocation s) (Set.delete who akLocation))
+             (Set.insert k akKeys)
+           , fkSteps s
+           , 0) ]
+
+data FindKey = FindKey
+  { fkSteps    :: !Int
+  , fkLocation :: !Coord
   }
   deriving Show
 
-at = Map.findWithDefault '#'
-
-done keyN ss = keyN == Set.size (keys ss)
-
-ss0 = SS 0 Set.empty
-
-step1 world SS{..} =
-  do here <- cardinal location
-     case at here world of
-       '#' -> []
-       '.' -> [SS (steps+1) keys here others]
-       '@' -> [SS (steps+1) keys here others]
-       a | isLower a ->
-             do let everyone = here : others
-                leader <- everyone
-                [SS (steps+1) (Set.insert (toUpper a) keys) leader (delete leader everyone)]
-         | isUpper a -> [SS (steps+1) keys here others | Set.member a keys ]
+nextKeys :: UArray Coord Char -> Set Char -> Coord -> [ (FindKey, Char) ]
+nextKeys world found start =
+  [ (s,k')
+      | s <- bfsOn fkLocation stepFK (FindKey 0 start)
+      , let k = world ! fkLocation s
+      , isLower k
+      , let k' = toUpper k
+      , Set.notMember k' found ]
+  where
+    stepFK FindKey{..} =
+      do guard (let k = world ! fkLocation in
+                k < 'a' || 'z' < k || Set.member (toUpper k) found)
+         here <- cardinal fkLocation
+         case world ! here of
+           '#' -> []
+           a | 'A' <= a, a <= 'Z', Set.notMember a found -> []
+           _ -> [FindKey (fkSteps+1) here]
