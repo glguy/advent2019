e0d866b9244ce15d032d46e650e91b37fdde5121
Eric Mertens <emertens@gmail.com>
2019-12-22 09:33:48 -0800

    Update Day22.hs

--- execs/Day22.hs
+++ execs/Day22.hs
@@ -8,28 +8,30 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2019/day/22>
 
->>> driver2 [DealNew] 1 10 <$> [0..9]
+>>> let shuffleTest cmds = runModFn (apply (commandsToLinear cmds)) 10 <$> [0..9]
+
+>>> shuffleTest [DealNew]
 [9,8,7,6,5,4,3,2,1,0]
 
->>> driver2 [Cut 3] 1 10 <$> [0..9]
+>>> shuffleTest [Cut 3]
 [3,4,5,6,7,8,9,0,1,2]
 
->>> driver2 [Cut (-4)] 1 10 <$> [0..9]
+>>> shuffleTest [Cut (-4)]
 [6,7,8,9,0,1,2,3,4,5]
 
->>> driver2 [DealInc 3] 1 10 <$> [0..9]
+>>> shuffleTest [DealInc 3]
 [0,7,4,1,8,5,2,9,6,3]
 
->>> driver2 [DealInc 7, DealNew, DealNew] 1 10 <$> [0..9]
+>>> shuffleTest [DealInc 7, DealNew, DealNew]
 [0,3,6,9,2,5,8,1,4,7]
 
->>> driver2 [Cut 6, DealInc 7, DealNew] 1 10 <$> [0..9]
+>>> shuffleTest [Cut 6, DealInc 7, DealNew]
 [3,0,7,4,1,8,5,2,9,6]
 
->>> driver2 [DealInc 7, DealInc 9, Cut (-2)] 1 10 <$> [0..9]
+>>> shuffleTest [DealInc 7, DealInc 9, Cut (-2)]
 [6,3,0,7,4,1,8,5,2,9]
 
->>> driver2 [DealNew, Cut (-2), DealInc 7, Cut 8, Cut (-4), DealInc 7, Cut 3, DealInc 9, DealInc 3, Cut (-1)] 1 10 <$> [0..9]
+>>> shuffleTest [DealNew, Cut (-2), DealInc 7, Cut 8, Cut (-4), DealInc 7, Cut 3, DealInc 9, DealInc 3, Cut (-1)]
 [9,2,5,8,1,4,7,0,3,6]
 
 -}
@@ -46,68 +48,82 @@ import Math.NumberTheory.Moduli.Class (Mod, getNatVal)
 -- Parsing
 ------------------------------------------------------------------------
 
-data Command
-  = Cut     Integer
-  | DealInc Integer
-  | DealNew
+data Technique
+  = Cut     Integer -- ^ cut N cards
+  | DealInc Integer -- ^ deal with increment N
+  | DealNew         -- ^ deal into new stack
   deriving Show
 
-parseCommand :: Parser Command
-parseCommand
-    = Cut     <$ "cut " <*> number
+parseTechnique :: Parser Technique
+parseTechnique
+    = Cut     <$ "cut "                 <*> number
   <|> DealInc <$ "deal with increment " <*> number
   <|> DealNew <$ "deal into new stack"
 
 ------------------------------------------------------------------------
--- Composite shuffles
+-- Shuffles
 ------------------------------------------------------------------------
 
-toComposite :: KnownNat n => Command -> Composite n
-toComposite DealNew     = Composite (-1) 1
-toComposite (Cut     i) = Composite 1 (fromInteger i)
-toComposite (DealInc i) = Composite (fromInteger i) 0
+-- | Compute function for a shuffle instruction mapping indexes in
+-- the shuffled deck to positions in the source deck.
+toLinear :: KnownNat n => Technique -> Linear (Mod n)
+toLinear DealNew     = Linear (-1) (-1)
+toLinear (Cut     i) = Linear 1 (fromInteger i)
+toLinear (DealInc i) = Linear (1 / fromInteger i) 0
 
-data Composite n = Composite
-  { compDealInc :: !(Mod n)
-  , compCut     :: !(Mod n)
-  }
+-- | Construts the linear function corresponding to applying the
+-- given shuffles in order from left to right.
+commandsToLinear :: KnownNat n => [Technique] -> Linear (Mod n)
+commandsToLinear = foldMap toLinear
+
+------------------------------------------------------------------------
+-- Linear functions
+------------------------------------------------------------------------
+
+-- | Linear functions: @Linear a b ~ λx. ax+b@
+data Linear a = Linear !a !a
   deriving Show
 
-apply :: KnownNat n => Composite n -> Mod n -> Mod n
-apply c i = i * compDealInc c - compCut c
+apply :: Num a => Linear a -> a -> a
+apply (Linear a b) x = a * x + b
 
-invert :: KnownNat n => Composite n -> Composite n
-invert (Composite a b) = Composite (recip a) (-b/a)
+invert :: Fractional a => Linear a -> Linear a
+invert (Linear a b) = Linear (1/a) (-b/a)
 
-instance KnownNat n => Semigroup (Composite n) where
-  Composite a b <> Composite c d = Composite (a*c) (c*b+d)
+-- | Composition of linear functions
+--
+-- >>> let f = Linear 1 2
+-- >>> let g = Linear 3 4
+-- >>> (f <> g) `apply` 10
+-- 36
+-- >>> f `apply` (g `apply` 10)
+-- 36
+instance Num a => Semigroup (Linear a) where
+  Linear a b <> Linear c d = Linear (a*c) (b + a*d)
 
-instance KnownNat n => Monoid (Composite n) where
-  mempty = Composite 1 0
+-- | @mempty ~ λx. x@
+instance Num a => Monoid (Linear a) where
+  mempty = Linear 1 0
 
 ------------------------------------------------------------------------
 -- Driver code
 ------------------------------------------------------------------------
 
--- y = ax-b
--- y + b = ax
--- y/a + b/a = x
-
 main :: IO ()
 main =
-  do inp <- getParsedLines 22 parseCommand
+  do inp <- getParsedLines 22 parseTechnique
      print (driver1 inp                           10007 2019)
      print (driver2 inp 101741582076661 119315717514047 2020)
 
-driver1 :: [Command] -> Natural -> Natural -> Natural
+driver1 :: [Technique] -> Natural -> Natural -> Natural
 driver1 commands =
-  runModFn (apply (foldMap toComposite commands))
+  runModFn (apply (invert (commandsToLinear commands)))
 
-driver2 :: [Command] -> Natural -> Natural -> Natural -> Natural
+driver2 :: [Technique] -> Natural -> Natural -> Natural -> Natural
 driver2 commands iterations =
-  runModFn (apply (invert (stimes iterations (foldMap toComposite commands))))
+  runModFn (apply (stimes iterations (commandsToLinear commands)))
 
 runModFn :: (forall n. KnownNat n => Mod n -> Mod n) -> Natural -> Natural -> Natural
 runModFn f modulus =
   case someNatVal modulus of
-    SomeNat (_ :: p m) -> getNatVal . f @m . fromIntegral
+    SomeNat (_ :: p m) -> getNatVal @m . f . fromIntegral
