1ab0543bdd6d4502ce71c1b6c5ed416fc9eaadd4
Eric Mertens <emertens@gmail.com>
2019-12-23 19:46:36 -0800

    Update Day23.hs

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -21,11 +21,9 @@ main :: IO ()
 main =
   do [pgm] <- getParsedLines 23 memoryParser
      let events = startup pgm
-     print (head       [y | SetY  y <- events])
-     print (firstMatch [y | SendY y <- events])
-
-firstMatch :: Eq a => [a] -> a
-firstMatch ys = head [ a | (a,b) <- zip ys (tail ys), a==b ]
+     print (head     [y | SetY  y <- events])
+     print (firstDup [y | SendY y <- events])
+     mapM_ print $ take 200 events
 
 data Packet = Packet !Int !Int !Int -- ^ destination, x, y
 
@@ -34,20 +32,28 @@ type Network = IntMap Effect
 
 -- | State of network simulation including most recent NAT packet.
 data System = System
-  { network :: Network
-  , nat     :: Maybe (Int,Int)
-  , sendq   :: Queue Packet
+  { network :: Network         -- ^ VMs indexed by identity
+  , nat     :: Maybe (Int,Int) -- ^ last NAT packet registered
+  , sendq   :: Queue Packet    -- ^ Sent packet queue
   }
 
 -- | Network events needed to answer part 1 and 2.
 data Event
   = SetY  !Int -- ^ Y value sent to address 255
-  | SendY !Int -- ^ NAT packet set after a system stall
+  | SendY !Int -- ^ NAT packet sent after a system stall
+  deriving Show
+
 
 -- | Run a VM gathering packets until it blocks waiting for input.
 gather :: Effect -> ([Packet], Effect)
 gather (Output dst (Output x (Output y e))) = do ([Packet dst x y], ()); gather e
 gather e                                    = pure e
+
+stepNetwork :: (Network -> ([Packet], Network)) -> System -> [Event]
+stepNetwork f sys =
+  case f (network sys) of
+    (ps, net) -> tryToSend sys{ network = net, sendq = Queue.appendList ps (sendq sys) }
+
 
 -- gather up any packets ready to send at the outset
 startup :: [Int] -> [Event]
@@ -75,10 +81,11 @@ deliver (Packet dst x y) sys
   | dst == 255 = SetY y : tryToSend sys{ nat = Just (x,y) }
   | otherwise  = stepNetwork (updateF (gather . feedInput [x,y]) dst) sys
 
-stepNetwork :: (Network -> ([Packet], Network)) -> System -> [Event]
-stepNetwork f sys =
-  case f (network sys) of
-    (ps, net) -> tryToSend sys{ network = net, sendq = Queue.appendList ps (sendq sys) }
+
+
 
 updateF :: Applicative f => (a -> f a) -> Int -> IntMap a -> f (IntMap a)
 updateF = IntMap.alterF . traverse
+
+firstDup :: Eq a => [a] -> a
+firstDup ys = head [ a | (a,b) <- zip ys (tail ys), a==b ]
