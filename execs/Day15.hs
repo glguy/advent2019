c61e89cf3c50a39e9dac2f6121ebc852b40af23e
Eric Mertens <emertens@gmail.com>
2020-12-31 14:38:58 -0800

    Update Day15.hs

--- execs/Day15.hs
+++ execs/Day15.hs
@@ -1,4 +1,4 @@
-{-# Language RecordWildCards #-}
+{-# Language RecordWildCards, ViewPatterns #-}
 {-|
 Module      : Main
 Description : Day 15 solution
@@ -19,15 +19,15 @@ import Intcode       (Effect(..), run, new)
 main :: IO ()
 main =
   do intcode <- getIntcodeInput 15
-     let part1:_ = filter onOxygen (explore (newSearchState intcode))
+     let part1:_ = filter onOxygen (search (newSearchState intcode))
      print (distance part1)
-     print (distance (last (explore part1{distance = 0})))
+     print (distance (last (search part1{distance = 0})))
 
 data SearchState = SearchState
   { onOxygen :: !Bool  -- ^ Is the robot currently on the oxygen
   , distance :: !Int   -- ^ Commands issued so far
   , location :: !Coord -- ^ robot's current location
-  , effect   :: Effect -- ^ robot control program state
+  , effect   :: Effect -- ^ robot control program
   }
 
 -- | Initial search state starting from assumed non-oxygen at the origin.
@@ -35,15 +35,18 @@ newSearchState :: [Int] {- ^ intcode -} -> SearchState
 newSearchState = SearchState False 0 origin . run . new
 
 -- | Breadth-first exploration of the maze
-explore :: SearchState -> [SearchState]
-explore = bfsOn location singleStep
+search :: SearchState -> [SearchState]
+search = bfsOn location searchStep
 
 -- | Generate the list of single steps that can be taken from a particular position.
-singleStep :: SearchState -> [SearchState]
-singleStep SearchState{..} =
+searchStep :: SearchState -> [SearchState]
+searchStep SearchState{..} =
   [ SearchState (o == 2) (distance + 1) (move location) e
-    | Input f     <- [effect]
-    , (i,move)    <- [(1,above),(2,below),(3,left),(4,right)]
-    , Output o e  <- [f i]
-    , o > 0
-    ]
+  | (i,move) <- [(1,above),(2,below),(3,left),(4,right)]
+  , (o,e)    <- effectStep i effect
+  ]
+
+-- | Give the robot a movement instruction and get the block status
+effectStep :: Int {- ^ direction -} -> Effect -> [(Int, Effect)]
+effectStep i (Input (($ i) -> Output o e)) | o > 0 = [(o,e)]
+effectStep _ _                                     = []
