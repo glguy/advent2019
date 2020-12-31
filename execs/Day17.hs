9e33e08da47a00cea7e1501ae1217ef8d3248f3d
Eric Mertens <emertens@gmail.com>
2020-12-31 09:41:48 -0800

    Update Day17.hs

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -1,3 +1,4 @@
+{-# Language BlockArguments, ImportQualifiedPost, TransformListComp #-}
 {-|
 Module      : Main
 Description : Day 17 solution
@@ -10,84 +11,99 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import           Advent
-import           Advent.Coord
-import           Data.Char
-import           Data.List
-import           Data.Map (Map)
-import qualified Data.Map as Map
-import           Data.Maybe
-import           Intcode
+import Advent (getIntcodeInput)
+import Advent.Coord
+import Data.Char (chr, ord)
+import Data.List (intersperse, stripPrefix, intercalate, inits, tails)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (maybeToList)
+import Intcode (intcodeToList, effectList, run, new, set)
 
+--
+-- >>> :main
+-- 7280
+-- 1045393
 main :: IO ()
 main =
   do inp <- getIntcodeInput 17
-     let ascii = map (chr . fromIntegral) (intcodeToList inp [])
+     let ascii = map chr (intcodeToList inp [])
          world = Map.fromList [ (C y x, col)
                                 | (y,row) <- zip [0..] (lines ascii)
-                                , (x,col) <- zip [0..] row ]
+                                , (x,col) <- zip [0..] row, col /= '.' ]
 
-     print $ sum [ coordRow k * coordCol k
-                 | k <- Map.keys world
-                 , all (\x -> '#' == at x world) (k : cardinal k) ]
-
+     print (part1 world)
      print (part2 inp world)
 
-at :: Coord -> Map Coord Char -> Char
-at = Map.findWithDefault '.'
+part1 :: Map Coord Char -> Int
+part1 world =
+  sum [ coordRow k * coordCol k
+      | k <- Map.keys world
+      , all (`Map.member` world) (cardinal k) ]
 
-part2 :: [Int] -> Map Coord Char -> Int
-part2 inp world =
-     last $ effectList (run (set 0 2 (new inp)))
-          $ map (fromIntegral . ord) input
+part2 ::
+  [Int]          {- ^ intcode program      -} ->
+  Map Coord Char {- ^ world map            -} ->
+  Int            {- ^ final program output -}
+part2 inp world = last (effectList (run (set 0 2 (new inp))) (map ord input))
   where
-    start   = head [ k | (k,'^') <- Map.toList world ]
-    p       = path world start (C (-1) 0)
-    input:_ = compress [] Nothing Nothing Nothing p
+    start = head [k | (k,'^') <- Map.toList world]
+    p     = path world start north
+    input = head (search 'A' [] [] p)
 
-data Dir = U | D | L | R
-  deriving (Eq, Ord, Show)
+data Dir = L | R deriving (Eq, Show)
+type Cmds = [(Dir,Int)]
 
-path :: Map Coord Char -> Coord -> Coord -> [(Dir,Int)]
+-- Determine that path that takes us from the start to the end
+path ::
+  Map Coord Char {- ^ world map        -} ->
+  Coord          {- ^ current location -} ->
+  Coord          {- ^ travel vector    -} ->
+  Cmds
 path world here dir
-  | '#' == at (addCoord (turnLeft  dir) here) world = walk L turnLeft
-  | '#' == at (addCoord (turnRight dir) here) world = walk R turnRight
-  | otherwise                                           = []
+  | Map.member (addCoord (turnLeft  dir) here) world = walk L turnLeft
+  | Map.member (addCoord (turnRight dir) here) world = walk R turnRight
+  | otherwise                                        = []
   where
     walk cmd f = (cmd, n) : path world endPoint dir'
       where
         dir'     = f dir
-        steps    = takeWhile (\x -> at x world /= '.')
+        steps    = takeWhile (`Map.member` world)
                  $ iterate (addCoord dir') here
         n        = length steps - 1
         endPoint = last steps
 
-compress ::
-  [String] ->
-  Maybe [(Dir,Int)] ->
-  Maybe [(Dir,Int)] ->
-  Maybe [(Dir,Int)] ->
-  [(Dir,Int)] ->
-  [String]
-compress acc a b c [] = [unlines [ intercalate "," (reverse acc)
-                                 , maybe "L" instructions a
-                                 , maybe "L" instructions b
-                                 , maybe "L" instructions c, "n"] ]
-compress acc a b c xs =
-  do (ys,zs) <- splits xs
-     [ () | short ys ]
-     id [ z | Just ys == a, z <- compress ("A":acc) a b c zs ] ++
-        [ z | Just ys == b, z <- compress ("B":acc) a b c zs ] ++
-        [ z | Just ys == c, z <- compress ("C":acc) a b c zs ] ++
-        [ z | isNothing a,                           z <- compress ("A":acc) (Just ys) b c zs ] ++
-        [ z | isJust    a, isNothing b,              z <- compress ("B":acc) a (Just ys) c zs ] ++
-        [ z | isJust    a, isJust    b, isNothing c, z <- compress ("C":acc) a b (Just ys) zs ]
 
-short :: [(Dir,Int)] -> Bool
+search ::
+  Char     {- ^ next subroutine name  -} ->
+  [Cmds]   {- ^ current subroutines   -} ->
+  [Char]   {- ^ reversed main program -} ->
+  Cmds     {- ^ remaining path        -} ->
+  [String] {- ^ program input         -}
+search _ subs pgm [] =
+  [ unlines
+  $ intersperse ',' (reverse pgm)
+  : take 3 (map instructions subs ++ repeat "L")
+  ++ ["n"]
+  ]
+search next subs pgm p = oldSub ++ newSub
+  where
+    oldSub = [ answer
+             | (name, sub) <- zip ['A'..] subs
+             , path'       <- maybeToList (stripPrefix sub p)
+             , answer      <- search next subs (name:pgm) path' ]
+
+    newSub = [ answer
+             | next < 'D'
+             , (sub,rest)  <- tail (splits p), then takeWhile by short sub
+             , answer      <- search (succ next) (subs ++ [sub]) (next:pgm) rest ]
+
+-- subroutines are limited to be at most 20 characters
+short :: Cmds -> Bool
 short xs = length (instructions xs) <= 20
 
-instructions :: [(Dir,Int)] -> String
-instructions xs = intercalate "," [ show d ++ "," ++ show n | (d,n) <- xs ]
+instructions :: Cmds -> String
+instructions xs = intercalate "," do (d,n) <- xs; [show d, show n]
 
 splits :: [a] -> [([a],[a])]
-splits xs = tail $ zip (inits xs) (tails xs)
+splits xs = zip (inits xs) (tails xs)
