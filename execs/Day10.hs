2a17b7dafce24a5454c869672d79ef6d8bbdfa06
Eric Mertens <emertens@gmail.com>
2021-01-01 20:40:40 -0800

    Update Day10.hs

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -1,3 +1,4 @@
+{-# Language ImportQualifiedPost #-}
 {-|
 Module      : Main
 Description : Day 10 solution
@@ -10,66 +11,50 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import           Advent
-import           Advent.Coord
-import           Data.List
-import           Data.Foldable
-import           Data.Set (Set)
-import qualified Data.Set as Set
-import           Data.Ord (comparing)
-import           Data.Ratio ((%))
+import Advent
+import Advent.Coord
+import Data.List (maximumBy, sortOn, transpose)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Ord (comparing)
+import Data.Ratio ((%))
 
 main :: IO ()
 main =
   do inp <- getInputLines 10
+     let (base, byAngles) = findTheBase [c | (c,'#') <- coordLines inp]
+     print (Map.size byAngles) -- part 1
+     let C y x = spiralOrder base byAngles !! 199
+     print (x * 100 + y) -- part 2
 
-     let m = Set.fromList [ c | (c,'#') <- coordLines inp ]
+collectBy :: Ord k => (a -> k) -> [a] -> Map k [a]
+collectBy f xs = Map.fromListWith (++) [(f x, [x]) | x <- xs]
 
-     let (base, vis) = maximumBy (comparing snd)
-                       [ (i, count (visible m i) m) | i <- toList m ]
-     print vis
-     let C y x = part2 base (Set.delete base m) !! 199
-     print (x * 100 + y)
+-- Given a list of asteroid locations, return the location of the
+-- asteroid with the largest collection of unique angles to other
+-- asteroids
+findTheBase :: [Coord] -> (Coord, Map Angle [Coord])
+findTheBase world =
+  maximumBy (comparing (Map.size . snd))
+    [(i, collectBy (angle . sub i) xs) | (i,xs) <- pickOne world]
 
-part2 :: Coord -> Set Coord -> [Coord]
-part2 base m
-  | Set.null m = []
-  | otherwise  = these ++ part2 base (m Set.\\ Set.fromList these)
-  where
-    these = filter (visible m base) (sortOn (toAngle . sub base) (toList m))
+spiralOrder :: Coord -> Map Angle [Coord] -> [Coord]
+spiralOrder base byAngles =
+  concat (transpose (map (sortOn (manhattan base)) (Map.elems byAngles)))
 
+-- 'subtract' but for 'Coord'
 sub :: Coord -> Coord -> Coord
 sub (C y x) (C v u) = C (v-y) (u-x)
 
--- Angle measure that sorts clockwise starting from 12 o'clock
---
--- >>> let ordered = [C (-1) 0,C (-1) 1,C 0 1,C 1 1,C 1 0,C 1 (-1),C 0 (-1),C (-1) (-1)]
--- >>> sortOn angle ordered == ordered
--- True
--- angle :: Coord -> Double
--- angle (C y x) = - atan2 (fromIntegral x) (fromIntegral y)
-
-visible :: Set Coord -> Coord -> Coord -> Bool
-visible _ x y | x == y = False
-visible ast (C y x) (C v u) =
-  and [ Set.notMember (C (v + stepy * i) (u + stepx * i)) ast | i <- [1 .. steps-1] ]
-  where
-    dx = x - u
-    dy = y - v
-    steps = gcd dx dy
-
-    stepx = dx `div` steps
-    stepy = dy `div` steps
-
 data Angle = Angle !Int !Rational -- quadrant and slope
-  deriving (Eq, Ord)
+  deriving (Show, Eq, Ord)
 
-toAngle :: Coord -> Angle
-toAngle (C y x)
+angle :: Coord -> Angle
+angle (C y x)
   | x == 0, y == 0 = Angle 0 0
   | x >= 0, y < 0  = mk 1 x (-y)    -- upper right
+  | y >= 0, x > 0  = mk 2 y x       -- lower right
   | x <= 0, y > 0  = mk 3 (-x) y    -- lower left
-  | y >= 0         = mk 2 y x       -- lower right
   | otherwise      = mk 4 (-y) (-x) -- upper left
   where
      mk i a b = Angle i (fromIntegral a % fromIntegral b)
