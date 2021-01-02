bc43b3c50fb8b9ceb0b1e90ce84241b89b45b4d0
Eric Mertens <emertens@gmail.com>
2021-01-01 19:17:03 -0800

    Update Day08.hs

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,4 +1,4 @@
-{-# Language OverloadedStrings #-}
+{-# Language ImportQualifiedPost #-}
 {-|
 Module      : Main
 Description : Day 8 solution
@@ -11,46 +11,33 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent              (Parser, getParsedLines, count)
-import Control.Applicative ((<|>),many)
-import Data.List           (minimumBy)
-import Data.Ord            (comparing)
-
-type Layer = [[Pixel]]
-
-data Pixel = Black | White | Transparent
-  deriving (Eq, Ord, Show)
-
-parsePixel :: Parser Pixel
-parsePixel = Black <$ "0" <|> White <$ "1" <|> Transparent <$ "2"
+import Advent (getInputLines, cardinality, chunks)
+import Data.List (minimumBy)
+import Data.Ord (comparing)
+import Data.Map qualified as Map
 
 main :: IO ()
 main =
-  do [inp] <- getParsedLines 8 (many parsePixel)
-     let layers = chunks 6 (chunks 25 inp)
+  do [inp] <- getInputLines 8
+     let layers = chunks (25*6) inp
      print (part1 layers)
-     mapM_ (putStrLn . map render) (overlayLayers layers)
+     mapM_ (putStrLn . map render) (chunks 25 (overlayLayers layers))
 
-render :: Pixel -> Char
-render Black       = '\x2591'
-render Transparent = '\x2592'
-render White       = '\x2588'
+render :: Char -> Char
+render '0' = '\x2591'
+render '1' = '\x2588'
+render '2' = '\x2592'
+render _   = error "bad pixel"
 
-overlayLayers :: [Layer] -> Layer
-overlayLayers = foldr1 (zipWith (zipWith overlay))
+overlayLayers :: [String] -> String
+overlayLayers = foldr1 (zipWith overlay)
 
-overlay :: Pixel -> Pixel -> Pixel
-overlay Transparent x = x
-overlay x           _ = x
+overlay :: Char -> Char -> Char
+overlay '2' x = x
+overlay x   _ = x
 
-part1 :: [Layer] -> Int
-part1 layers = count (White==) layer * count (Transparent==) layer
+part1 :: [String] -> Int
+part1 = checksum . minimumBy (comparing (ix '0')) . map cardinality
   where
-    layer = minimumBy (comparing (count (Black==)))
-          $ map concat layers
-
-chunks :: Int -> [a] -> [[a]]
-chunks _ [] = []
-chunks n xs =
-  case splitAt n xs of
-    (a,b) -> a : chunks n b
+    ix         = Map.findWithDefault 0
+    checksum x = ix '1' x * ix '2' x
