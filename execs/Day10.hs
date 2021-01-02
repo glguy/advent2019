782ec0a4dbc403f933410171705b262c0e3d4d1b
Eric Mertens <emertens@gmail.com>
2021-01-01 20:53:14 -0800

    Update Day10.hs

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -17,7 +17,6 @@ import Data.List (maximumBy, sortOn, transpose)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Ord (comparing)
-import Data.Ratio ((%))
 
 main :: IO ()
 main =
@@ -30,30 +29,20 @@ main =
 -- Given a list of asteroid locations, the other asteroids
 -- arranged by their angle from the best base in order of
 -- distance from that base.
-findTheBase :: [Coord] -> Map Angle [Coord]
+findTheBase :: [Coord] -> Map Double [Coord]
 findTheBase world =
   maximumBy (comparing Map.size)
   [sortOn (manhattan i) <$> collectBy (angle . sub i) xs | (i,xs) <- pickOne world]
 
-spiralOrder :: Map Angle [Coord] -> [Coord]
+spiralOrder :: Map Double [Coord] -> [Coord]
 spiralOrder = concat . transpose . Map.elems
 
 -- 'subtract' but for 'Coord'
 sub :: Coord -> Coord -> Coord
 sub (C y x) (C v u) = C (v-y) (u-x)
 
-data Angle = Angle !Int !Rational -- quadrant and slope
-  deriving (Show, Eq, Ord)
-
-angle :: Coord -> Angle
-angle (C y x)
-  | x == 0, y == 0 = Angle 0 0
-  | x >= 0, y < 0  = mk 1 x (-y)    -- upper right
-  | y >= 0, x > 0  = mk 2 y x       -- lower right
-  | x <= 0, y > 0  = mk 3 (-x) y    -- lower left
-  | otherwise      = mk 4 (-y) (-x) -- upper left
-  where
-     mk i a b = Angle i (fromIntegral a % fromIntegral b)
+angle :: Coord -> Double
+angle (C y x) = atan2 (- fromIntegral x) (fromIntegral y)
 
 -- | Given a characterizing function arrange elements that
 -- have the same characterization.
