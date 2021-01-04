13f3be89535c12cbae761a6d4165432a2459ccd5
Eric Mertens <emertens@gmail.com>
2021-01-03 16:57:43 -0800

    Update Day14.hs

--- execs/Day14.hs
+++ execs/Day14.hs
@@ -11,13 +11,12 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent (Parser, satisfy, number, sepBy, getParsedLines)
+import Advent (Parser, getParsedLines, number, satisfy, sepBy)
 import Control.Applicative (some)
 import Data.List (foldl', sortOn)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Char (isAlpha)
-import Data.Ratio ((%))
 
 p2ore :: Integer
 p2ore = 1_000_000_000_000
@@ -43,56 +42,58 @@ parseReaction = (,) <$> parseItems <* " => " <*> parseItem
 
 type Step = (String, Integer, Map String Integer)
 
+-- |
+-- :main
+-- 751038
+-- 2074843
 main :: IO ()
 main =
   do inp <- getParsedLines 14 parseReaction
      let recipes = arrange inp
 
-     print (oreNeeded recipes 1)
+     print (oreNeeded divUp recipes 1)
 
-     let optimal = optimalOrePerFuel recipes
-     print $ until (\i -> oreNeeded recipes i <= p2ore) (subtract 1)
-           $ truncate (fromIntegral p2ore / optimal)
-
--- | Compute the amount of ORE needed to construct a single FUEL when
--- fractional units are possible.
-optimalOrePerFuel :: [Step] -> Rational
-optimalOrePerFuel r = orePerThing Map.! "FUEL"
-  where
-    orePerThing = Map.fromList (("ORE",1):[(c, aux b a) | (c,b,a) <- r])
-    aux n needs = sum (Map.intersectionWith (\x y -> x * (y%n)) orePerThing needs)
+     let optimal = fromInteger p2ore / oreNeeded (/) recipes 1 :: Rational
+     print $ until (\i -> oreNeeded divUp recipes i <= p2ore) (subtract 1)
+           $ truncate optimal
 
 -- | Determine the amount of ORE needed to create the given amount of FUEL
 oreNeeded ::
-  [Step]  {- ^ arranged reaction steps -} ->
-  Integer {- ^ fuel amount -} ->
-  Integer {- ^ ore amount  -}
-oreNeeded recipes n = foldl' step (Map.singleton "FUEL" n) recipes Map.! "ORE"
+  Num a =>
+  (a -> a -> a) {- ^ division                -} ->
+  [Step]        {- ^ arranged reaction steps -} ->
+  a             {- ^ fuel amount             -} ->
+  a             {- ^ ore amount              -}
+oreNeeded divide recipes n =
+  foldl' (doStep divide) (Map.singleton "FUEL" n) recipes Map.! "ORE"
 
 -- | Use the given reaction step to reduce any of that item needed into
 -- simpler components.
-step ::
-  Map String Integer {- ^ items needed before reaction -} ->
-  Step               {- ^ reaction step                -} ->
-  Map String Integer {- ^ items needed after reaction  -}
-step need (item, makes, needs) =
+doStep ::
+  Num a =>
+  (a -> a -> a) {- ^ division                     -} ->
+  Map String a  {- ^ items needed before reaction -} ->
+  Step          {- ^ reaction step                -} ->
+  Map String a  {- ^ items needed after reaction  -}
+doStep divide need (item, makes, needs) =
   case Map.lookup item need of
     Nothing  -> need
     Just qty -> Map.unionWith (+) need1 need2
       where
-        reactions = qty `divUp` makes
+        reactions = qty `divide` fromInteger makes
         need1 = Map.delete item need
-        need2 = (reactions*) <$> needs
+        need2 = (\i -> reactions * fromInteger i) <$> needs
 
 -- | Sort the reaction steps topologically so that earlier steps only
 -- rely on later steps.
 arrange :: [Reaction] -> [Step]
-arrange xs = sortOn (negate . depth . (\(x,_,_)->x)) out
+arrange xs = sortOn (negate . depth . key) out
   where
-    out        = [(rhs,n,Map.fromList [(name,m)|(m,name)<-lhs]) | (lhs,(n,rhs)) <- xs]
-    depthMap   = Map.fromList (("ORE", 0::Int) : [(dst, toDepth src)  | (src,(_,dst)) <- xs])
+    key (x,_,_) = x
+    out        = [(rhs, n, Map.fromList [(name,m)|(m,name)<-lhs]) | (lhs,(n,rhs)) <- xs]
+    depthMap   = Map.fromList (("ORE", 0) : [(dst, toDepth src)  | (src,(_,dst)) <- xs])
     depth      = (depthMap Map.!)
-    toDepth [] = 0
+    toDepth [] = 0 :: Int
     toDepth ys = 1 + maximum (depth . snd <$> ys)
 
 -- | Integer division that rounds up instead of down.
